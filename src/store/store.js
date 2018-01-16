e1791cc966184a353b17632164dafb36f9fbc031
lanxing <569767186@qq.com>
2018-01-16 18:28:12 +0800

    vuex

--- src/store/store.js
+++ src/store/store.js
@@ -4,11 +4,13 @@ import Vuex from 'vuex';
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
+    strict: process.env.NODE_ENV !== 'production', // 如果 state 不是通过 mutation 修改的将报错
     modules: {},
+    plugins: [],
     state: {},
     getters: {},
     mutations: {},
     actions: {},
-})
+});
 
 export default store;
